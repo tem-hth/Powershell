30c043b35a33a92b72b6e1647ecb48ad0f881fcf
Tem Ogunfiditimi <tem@episof.com>
2018-10-02 15:27:24 -0400

    Added ISO Location

--- OSISOPreparation/Create-WindowsOSISO.ps1
+++ OSISOPreparation/Create-WindowsOSISO.ps1
@@ -14,9 +14,61 @@
 #>
 
 
-$WinADKOSCDIMGPath ="C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg"
-$WinInstallerSource = "C:\Deploy\OS\WinServer2016"
-$WinISODest = "C:\Deploy\ISO\WinServer2016.iso"
-Start-Process "$WinADKOSCDIMGPATH\oscdimg.exe" -Args "-m -o -u2 -udfver102 bootdata:2#p0,e,b$WinADKOSCDIMGPATH\etfsboot.com#pEF,e,b$WinADKOSCDIMGPATH\efisys.bin $WinInstallerSource $WinISODest"
+# Path to the Extracted or Mounted Windows ISO 
+$ISOMediaFolder = "C:\Deploy\OS\WinServer2016\"
+ 
+# Path to new re-mastered ISO
+$ISOFile = 'C:\Deploy\ISO\WinServer2016_App.iso'
+$ISOPath = "C:\Deploy\ISO"
+ 
+# Need to specify the root directory of the oscdimg.exe utility which you need to download
+$PathToOscdimg = 'C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg'
+ 
+# Instead of pointing to normal efisys.bin, use the *_noprompt instead
+$BootData='2#p0,e,b"{0}"#pEF,e,b"{1}"' -f "$ISOMediaFolder\boot\etfsboot.com","$ISOMediaFolder\efi\Microsoft\boot\efisys_noprompt.bin"
+
+if (Test-Path $ISOPath) {
+    if (Test-Path $ISOFile) {
+        Write-Host "ISO Already Exist." -ForegroundColor Red
+        Write-Host "Deleting old ISO" -ForegroundColor Yellow
+        Remove-Item $ISOFile
+    }
+    Write-Host "Creating ISO Image" -ForegroundColor Yellow 
+    # re-master Windows ISO
+    $createISOProcess = Start-Process -FilePath "$PathToOscdimg\oscdimg.exe" -ArgumentList @("-bootdata:$BootData",'-u2','-udfver102',"$ISOMediaFolder","$ISOFile") -PassThru -Wait -NoNewWindow
+    if (Test-Path $ISOFile) {
+        Write-Host "Image Creation Process Complete" -ForegroundColor Green
+    } else {
+        Write-Host "Unable to create ISO" -ForegroundColor Red
+    
+    }
+    
+} else  {
+    Write-Host "ISO Destination Path Does Not Exist" -ForegroundColor Red
+    Write-Host "Creating Destination Path: $ISOPath" -ForegroundColor Yellow
+    New-Item "$ISOPath" -ItemType Directory -Force
+    if (Test-Path $ISOPath) {
+        if (Test-Path $ISOFile) {
+        Write-Host "ISO Already Exist." -ForegroundColor Red
+        Write-Host "Deleting old ISO" -ForegroundColor Yellow
+        Remove-Item $ISOFile
+        }
+        Write-Host "Creating ISO Image" -ForegroundColor Yellow 
+        # re-master Windows ISO
+        $createISOProcess = Start-Process -FilePath "$PathToOscdimg\oscdimg.exe" -ArgumentList @("-bootdata:$BootData",'-u2','-udfver102',"$ISOMediaFolder","$ISOFile") -PassThru -Wait -NoNewWindow
+        if (Test-Path $ISOFile) {
+        Write-Host "Image Creation Process Complete" -ForegroundColor Green
+        } else {
+        Write-Host "Unable to create ISO" -ForegroundColor Red
+    
+        }
+        
+    
+    }
+
+}
+
+
+	
 
 
