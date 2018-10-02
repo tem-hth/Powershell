ed1d8e9aa42d16d98eff47627fbe4ff4a46a68d6
Tem Ogunfiditimi <tem@episof.com>
2018-10-02 15:27:51 -0400

    No Longer Needed

--- OSISOPreparation/Enable-FeatureDotNet35.ps1
+++
@@ -1,5 +0,0 @@
-cd "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\DISM"
-if not exist c:\offline md c:\offline
-dism /mount-wim /wimfile:c:\iso\sources\install.wim /index:1 /mountdir:c:\offline
-dism /image:c:\offline /enable-feature /featurename:NetFx3 /all /limitaccess /source:c:\iso\sources\sxs
-dism /unmount-wim /mountdir:c:\offline /commit
