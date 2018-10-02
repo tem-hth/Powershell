7ef14d7c9d03fbc574bf0b3a1fc777fe2f228e2a
Tem Ogunfiditimi <tem@episof.com>
2018-10-02 16:10:53 -0400

    Change execution order

--- DiscoveryManagerInstallation/Post-Installation.ps1
+++ DiscoveryManagerInstallation/Post-Installation.ps1
@@ -2,12 +2,11 @@ Invoke-Expression ".\Disable-IESec.ps1"
 Invoke-Expression ".\Disable-UAC.ps1"
 Invoke-Expression ".\Disable-Firewall.ps1"
 Invoke-Expression ".\Enable-RDP.ps1"
-Invoke-Expression ".\RenameComputerNametoVMName.ps1"
 Invoke-Expression ".\Install-DotNet.ps1"
 Invoke-Expression ".\Install-Chrome.ps1"
 Invoke-Expression ".\notepadplusplus_silentinstall.ps1"
 Invoke-Expression ".\Map-ShareDrive.ps1"
 Invoke-Expression ".\WinDefendExclude.ps1"
-
+Invoke-Expression ".\RenameComputerNametoVMName.ps1"
 
 
