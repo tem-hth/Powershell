b74168bec0ca300d97515360e82835b082863de4
Tem Ogunfiditimi <tem@episof.com>
2018-10-02 18:59:46 -0400

    Post Installation Script

---
+++ ApplianceInstallation/Post-Installation.ps1
@@ -0,0 +1,8 @@
+Invoke-Expression ".\Install-DellFirmwareUpdates.ps1"
+Invoke-Expression ".\Disable-IESec.ps1"
+Invoke-Expression ".\Disable-UAC.ps1"
+Invoke-Expression ".\Enable-RDP.ps1"
+Invoke-Expression ".\Instal-OMSA.ps1"
+Invoke-Expression ".\Install-Chrome.ps1"
+
+
