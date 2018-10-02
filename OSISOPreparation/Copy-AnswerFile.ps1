a2da1f441f3c1997c3264b9cb2d2e79225c1797b
Tem Ogunfiditimi <tem@episof.com>
2018-10-02 15:25:35 -0400

    Initial Commit

---
+++ OSISOPreparation/Copy-AnswerFile.ps1
@@ -0,0 +1,14 @@
+﻿$WindowsInstallerSource = "C:\Deploy\OS\WinServer2016"
+$AnswerFile = "..\..\AnswerFiles\WinServer2016DC\mesapp\autounattend.xml" 
+
+if(TestPath "$WindowsInstallerSource\autounattend.xml") {
+    Write-Host "Answer File already Exist" -ForegroundColor Red
+    Write-Host "Deleting old Answer File" -ForegroundColor Yellow
+    Remove-Item "$WindowsInstallerSource\autounattend.xml"
+    Write-Host "Copying Answer File" -ForegroundColor Green
+    Copy-Item $AnswerFile
+}
+else {
+    Write-Host "Copying Answer File" -ForegroundColor Green
+    Copy-Item $AnswerFile 
+}
