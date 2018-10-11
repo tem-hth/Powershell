d3878816fc9f6be8247d3b7f84c04635672ea20a
Tem Ogunfiditimi <tem@episof.com>
2018-10-11 17:19:04 -0400

    Added screen messages

--- ApplianceInstallation/Post-ISOInstallation.ps1
+++ ApplianceInstallation/Post-ISOInstallation.ps1
@@ -1,3 +1,5 @@
+Write-Host "Starting Post-OS Installation Phase" -ForegroundColor Green
+
 Invoke-Expression ".\Copy-DeployToCDrive.ps1"
 Invoke-Expression ".\Install-DellFirmwareUpdates.ps1"
 Invoke-Expression ".\Disable-IESec.ps1"
@@ -6,4 +8,6 @@ Invoke-Expression ".\Enable-RDP.ps1"
 Invoke-Expression ".\Instal-OMSA.ps1"
 Invoke-Expression ".\Install-Chrome.ps1"
 
-
+Write-Host "Post-OS Installation Phase Complete" -ForegroundColor Green
+Write-Host "Please Configure Team Nic and Hyper-V Switch" -ForegroundColor Green
+Write-Host "Then Use VM Build Scripts" -ForegroundColor Green
